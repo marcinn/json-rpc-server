a305bce71cdc6f33f8084e675bf04b7da32da7af
Marcin Nowak <marcin.j.nowak@gmail.com>
2025-11-04 01:01:46 +0100

    upgrade to python 3.13 + tests

--- jsonrpcserver/__init__.py
+++ jsonrpcserver/__init__.py
@@ -1,125 +1,142 @@
-import collections
-import json
-import inspect
-import six
+"""A simple JSON-RPC server."""
 
+import collections.abc
+import inspect
+import json
 import logging
+
 log = logging.getLogger(__name__)
 
 
 class BaseJsonRpcException(Exception):
+    """Base JSON-RPC exception"""
+
     code = -32603
 
     def __init__(self, message=None, data=None):
-        super(BaseJsonRpcException, self).__init__(message)
+        super().__init__(message)
         self.message = message
         self.data = data
 
 
 class AlreadyRegistered(Exception):
-    pass
+    """Method already registered"""
 
 
 class InvalidParametersException(BaseJsonRpcException):
+    """Invalid method parameters"""
+
     code = -32602
 
     def __init__(self, message=None, data=None):
-        super(InvalidParametersException, self).__init__(
-                                    message=message, data=data)
+        super().__init__(message=message, data=data)
 
 
 class RpcException(BaseJsonRpcException):
+    """Custom JSON-RPC exception"""
+
     def __init__(self, code, message=None, data=None):
-        if code >= -32768 and code <= -32000:
+        if -32768 <= code <= -32000:
             raise ValueError(
-                'Codes between (-32768, -32000) are reserved '
-                'for internal use only')
+                "Codes between (-32768, -32000) are reserved for internal use only"
+            )
         self.code = code
-        super(RpcException, self).__init__(message, data=data)
+        super().__init__(message, data=data)
 
 
-class Result(object):
-    def __init__(self, id, result):
-        self.version = '2.0'
-        self.id = id
+class Result:
+    """JSON-RPC success response"""
+
+    def __init__(self, id_, result):
+        self.version = "2.0"
+        self.id = id_
         self.result = result
 
     def as_dict(self):
+        """Return the response as a dictionary."""
         return {
-                'jsonrpc': self.version,
-                'id': self.id,
-                'result': self.result,
-                }
+            "jsonrpc": self.version,
+            "id": self.id,
+            "result": self.result,
+        }
 
 
-class Error(object):
-    def __init__(self, id, message, code, data=None):
-        self.version = '2.0'
-        self.id = id
+class Error:
+    """JSON-RPC error response"""
+
+    def __init__(self, id_, message, code, data=None):
+        self.version = "2.0"
+        self.id = id_
         self.message = message
         self.code = int(code)
         self.data = data
 
     def as_dict(self):
+        """Return the response as a dictionary."""
+
         error = {
-            'message': self.message,
-            'code': self.code,
-            }
+            "message": self.message,
+            "code": self.code,
+        }
 
         if self.data is not None:
-            error['data'] = self.data
+            error["data"] = self.data
 
         return {
-            'jsonrpc': self.version,
-            'id': self.id,
-            'error': error,
-            }
+            "jsonrpc": self.version,
+            "id": self.id,
+            "error": error,
+        }
 
 
 class MethodNotFoundError(Error):
-    def __init__(self, id, method, data=None):
-        super(MethodNotFoundError, self).__init__(
-            id, 'Method not found `%s`' % method, -32601, data=data)
+    """Method not found"""
+
+    def __init__(self, id_, method, data=None):
+        super().__init__(id_, f"Method not found `{method}`", -32601, data=data)
 
 
 class InvalidRequestError(Error):
-    def __init__(self, id, message, data=None):
-        super(InvalidRequestError, self).__init__(
-            id, message, -32600, data=data)
+    """Invalid JSON-RPC request"""
+
+    def __init__(self, id_, message, data=None):
+        super().__init__(id_, message, -32600, data=data)
 
 
 class ParseError(Error):
+    """Request is not a valid JSON"""
+
     def __init__(self, message, data=None):
-        super(ParseError, self).__init__(
-            None, 'Server received invalid JSON: %s' % message,
-            -32700, data=data)
+        super().__init__(
+            None, f"Server received invalid JSON: {message}", -32700, data=data
+        )
 
 
 class InvalidParametersError(Error):
-    def __init__(self, id, message=None, data=None):
-        super(InvalidParametersError, self).__init__(
-            id, message or 'Invalid parameters number or format',
-            -32602, data=data)
+    """Invalid method parameters"""
+
+    def __init__(self, id_, message=None, data=None):
+        super().__init__(
+            id_, message or "Invalid parameters number or format", -32602, data=data
+        )
 
 
 class InternalError(Error):
-    def __init__(self, id, message=None, data=None):
-        super(InternalError, self).__init__(
-            id, message or 'Internal Error', -32603, data=data)
+    """Internal server error"""
+
+    def __init__(self, id_, message=None, data=None):
+        super().__init__(id_, message or "Internal Error", -32603, data=data)
 
 
-class Service(object):
+class Service:
+    """JSON-RPC service"""
+
     def __init__(self):
         self._methods = {}
-        self.register('trait_names', self.trait_names)
-        self.register('_getAttributeNames', self.get_attribute_names)
+        self.register("trait_names", self.trait_names)
+        self.register("_getAttributeNames", self.get_attribute_names)
 
-        try:
-            getattr(inspect, 'getcallargs')  # Python 2.7+
-        except AttributeError:
-            self._validate_method_args = False  # Python 2.6 or older
-        else:
-            self._validate_method_args = True  # Python 2.7 or newer
+        self._validate_method_args = True
 
     def handle_http_request(self, request):
         """
@@ -135,139 +152,159 @@ class Service(object):
         return self.handle_request_body(request.body, request)
 
     def handle_request_body(self, body, http_request=None):
-        log.debug('Got request raw body: %s', body)
-
+        log.debug("Got request raw body: %s", body)
         request_dict = self.parse_request_body(body)
-        response = self.dispatch(request_dict, http_request)
+
+        if "error" in request_dict:
+            response = request_dict
+        else:
+            response = self.dispatch(request_dict, http_request)
 
         try:
-            response = json.dumps(response.as_dict()) if response else ''
-            log.debug('Sending raw response: %s', response)
+            if not response:
+                response = ""
+            elif isinstance(response, dict):
+                response = json.dumps(response)
+            else:
+                response = json.dumps(response.as_dict())
+            log.debug("Sending raw response: %s", response)
             return response
         except (TypeError, ValueError) as ex:
-            log.debug('Internal error: %s', ex)
-            return json.dumps(InternalError(
-                request_dict.get('id'), six.text_type(ex)).as_dict())
+            log.debug("Internal error: %s", ex)
+
+            return json.dumps(InternalError(request_dict.get("id"), str(ex)).as_dict())
 
     def parse_request_body(self, body):
+        """Parse request body and return a Python dictionary"""
+
         try:
             return json.loads(body)
         except (ValueError, TypeError) as ex:
-            log.debug('Parse error: %s', ex)
-            return ParseError(six.text_type(ex)).as_dict()
+            log.debug("Parse error: %s", ex)
+            return ParseError(str(ex)).as_dict()
 
     def dispatch(self, request, http_request=None):
-        ident = request.get('id')
+        """Find and call a method described in the request"""
 
-        log.debug('Dispatching request ID: %s', ident)
+        id_ = request.get("id", None)
+
+        log.debug("Dispatching request ID: %s", id_)
         try:
-            version = request['jsonrpc']
+            version = request["jsonrpc"]
         except KeyError:
-            log.debug('Missing `jsonrpc` key in request payload')
-            if ident:
-                return InvalidRequestError(
-                    ident, 'Missing `jsonrpc` key in request object')
-            else:
-                return
+            log.debug("Missing `jsonrpc` key in request payload")
+            return InvalidRequestError(id_, "Missing `jsonrpc` key in request object")
 
-        if not version == '2.0':
-            log.debug('Requested unsupported JSON-RPC version: %s', version)
-            if ident:
-                return InvalidRequestError(
-                    ident, 'Server supports only version 2.0 of '
-                           'the JSON-RPC protocol')
-            else:
-                return
+        if not version == "2.0":
+            log.debug("Requested unsupported JSON-RPC version: %s", version)
+            return InvalidRequestError(
+                id_,
+                "Server supports only version 2.0 of the JSON-RPC protocol",
+            )
 
         try:
-            method = request['method']
+            method = request["method"]
         except KeyError:
-            log.debug('Missing `method` key in request payload')
-            if ident:
-                return InvalidRequestError(
-                    ident, 'Missing `method` key in request object')
-            else:
-                return
+            log.debug("Missing `method` key in request payload")
+            return InvalidRequestError(id_, "Missing `method` key in request object")
 
         try:
-            log.debug('Calling method `%s`', method)
+            log.debug("Calling method `%s`", method)
             method = self._methods[method]
         except KeyError:
-            log.debug('Method not found: `%s`', method)
-            if ident:
-                return MethodNotFoundError(ident, method)
-            else:
-                return
+            log.debug("Method not found: `%s`", method)
+            return MethodNotFoundError(id_, method)
 
         args = []
         kwargs = {}
 
         try:
-            params = request['params']
+            params = request["params"]
         except KeyError:
             pass
         else:
             if params:
-                if isinstance(params, collections.Mapping):
-                    log.debug('Assuming params as a dictionary-like object')
+                if isinstance(params, collections.abc.Mapping):
+                    log.debug("Assuming params as a dictionary-like object")
                     kwargs = params
                 else:
-                    log.debug('Assuming params as a list-like object')
+                    log.debug("Assuming params as a list-like object")
                     args = params
 
-        if method['takes_http_request']:
-            args = [http_request]+args
+        if method["takes_http_request"]:
+            args = [http_request] + args
 
         if self._validate_method_args:
             try:
-                inspect.getcallargs(method['callback'], *args, **kwargs)
+                inspect.signature(method["callback"]).bind(*args, **kwargs)
             except TypeError as ex:
-                log.debug('Invalid method parameters: %s', ex)
-                if ident:
-                    return InvalidParametersError(
-                        ident, data=six.text_type(ex))
-                else:
-                    return
+                log.debug("Invalid method parameters: %s", ex)
+                return InvalidParametersError(id_, data=str(ex))
 
         try:
-            result = method['callback'](*args, **kwargs)
+            result = method["callback"](*args, **kwargs)
         except BaseJsonRpcException as ex:
-            return Error(ident, ex.message, ex.code, data=ex.data)
-        else:
-            return Result(ident, result) if ident else None
+            return Error(id_, ex.message, ex.code, data=ex.data)
+
+        return Result(id_, result)
 
     def method(self, method=None, takes_http_request=False):
+        """
+        Register a method
+
+        It can be used as a decorator.
+
+        :param method:             Method name
+        :param takes_http_request: Method will be given a HTTP request object
+                                   as a first argument
+        """
 
         if callable(method):
             self.register(method.__name__, method, takes_http_request)
             return method
-        else:
-            def wrapper(func):
-                self.register(
-                    method or func.__name__, func, takes_http_request)
-                return func
-            return wrapper
+
+        def wrapper(func):
+            self.register(method or func.__name__, func, takes_http_request)
+            return func
+
+        return wrapper
 
     def register(self, method, func, takes_http_request=False):
-        if method in self._methods:
-            raise AlreadyRegistered(
-                    'Method `%s` already registered.' % method)
+        """
+        Register a method
 
-        log.debug('Registering method `%s`', method)
+        :param method:             Method name
+        :param func:               A function to be called
+        :param takes_http_request: Method will be given a HTTP request object
+                                   as a first argument
+        """
+
+        if method in self._methods:
+            raise AlreadyRegistered(f"Method `{method}` already registered.")
+
+        log.debug("Registering method `%s`", method)
         self._methods[method] = {
-                'callback': func,
-                'takes_http_request': takes_http_request,
-                'argspec': inspect.getargspec(func),
-                }
+            "callback": func,
+            "takes_http_request": takes_http_request,
+            "argspec": inspect.getfullargspec(func),
+        }
 
     def trait_names(self):
+        """Return a list of public method names"""
+
         return self.public_methods().keys()
 
     def get_attribute_names(self):
+        """Used by XML-RPC, not implemented"""
+
         return []
 
     def public_methods(self):
-        return dict(filter(
-            lambda x: (not x[0].startswith('_')
-                       and not x[0] == 'trait_names'),
-            self._methods.items()))
+        """Return a dictionary of public methods"""
+
+        return dict(
+            filter(
+                lambda x: (not x[0].startswith("_") and not x[0] == "trait_names"),
+                self._methods.items(),
+            )
+        )
--- jsonrpcserver/introspection.py
+++ jsonrpcserver/introspection.py
@@ -1,34 +1,22 @@
-import sys
 import inspect
+from textwrap import dedent
 
 
 def trim_docstring(docstring):
+    """
+    Cut indentation from docstring
+
+    See http://www.python.org/dev/peps/pep-0257/
+    """
+
     if not docstring:
         return ''
-    # Convert tabs to spaces (following the normal Python rules)
-    # and split into a list of lines:
-    lines = docstring.expandtabs().splitlines()
-    # Determine minimum indentation (first line doesn't count):
-    indent = sys.maxint
-    for line in lines[1:]:
-        stripped = line.lstrip()
-        if stripped:
-            indent = min(indent, len(line) - len(stripped))
-    # Remove indentation (first line is special):
-    trimmed = [lines[0].strip()]
-    if indent < sys.maxint:
-        for line in lines[1:]:
-            trimmed.append(line[indent:].rstrip())
-    # Strip off trailing and leading blank lines:
-    while trimmed and not trimmed[-1]:
-        trimmed.pop()
-    while trimmed and not trimmed[0]:
-        trimmed.pop(0)
-    # Return a single string:
-    return '\n'.join(trimmed)
+    return dedent(docstring)
 
 
 def introspect(service, url):
+    """Create a structure describing the service"""
+
     methods = []
     for method, meta in service.public_methods().items():
         callback = meta['callback']
@@ -37,13 +25,11 @@ def introspect(service, url):
             'description': trim_docstring(callback.__doc__),
             'args': meta['argspec'].args,
             'invocation': '%s%s' % (method,
-                inspect.formatargspec(*meta['argspec'])),
+                str(inspect.signature(callback))),
             })
     return {
-            'methods': methods,
-            'url': url,
-            'name': getattr(service, '__name__', service.__class__.__name__),
-            'description': trim_docstring(service.__doc__),
-            }
-
-
+        'methods': methods,
+        'url': url,
+        'name': getattr(service, '__name__', service.__class__.__name__),
+        'description': trim_docstring(service.__doc__),
+    }
